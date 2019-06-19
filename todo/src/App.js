a41f198904d48095f4b63394967ee9a36c5d7656
Jason Belgard <belgard.jason@gmail.com>
2019-06-19 09:21:30 -0500

    MVP and delete button

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ToDoForm from './Components/ToDoForm'
+import ToDoList from './Components/ToDoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ToDoForm />
+        <ToDoList />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
---
+++ todo/src/Components/ToDoItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { deleteToDo, toggleCompleted } from '../actions'
+
+class ToDoItem extends Component {
+    constructor(props) {
+        super(props);
+    }
+    handleToggleCompleted = () => {
+        this.props.toggleCompleted(this.props.index);
+    };
+    render() {
+        return (
+            <div>
+                <h2
+                    onClick={this.handleToggleCompleted}
+                    stype={this.props.completed ? { color: 'red'} : null}
+                >
+                    {this.props.todo.value}
+                </h2>
+                <button onClick={() => this.props.deleteToDo(this.props.index)}>
+                    Delete!
+                </button>
+            </div>
+        );
+    }
+}
+export default connect(
+    null,
+    { toggleCompleted, deleteToDo }
+)(ToDoItem);
+
